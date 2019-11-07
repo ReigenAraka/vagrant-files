9250e4c04683711a4485a31e510158b03fbddf47
れいげん あらたか <boyan.wang@zilliz.com>
2019-11-07 11:47:59 +0800

    update shell install docker with ali mirror

--- gnes/bootstrap.sh
+++ gnes/bootstrap.sh
@@ -1,2 +1,18 @@
 echo "Install Docker..."
-sudo apt install docker -y
+# Step 1: Install some necessary system tools
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+# Step 2: Install the GPG certificate
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+# Step 3: Write the software source information
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# Step 4: Update and install Docker-CE
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+# Install Docker CE of a specified version:
+# Step 1: Search for the required version of Docker-CE:
+# apt-cache madison docker-ce
+#   docker-ce | 17.03.1~ce-0~ubuntu-xenial | http://mirrors.aliyun.com/docker-ce/linux/ubuntu xenial/stable amd64 Packages
+#   docker-ce | 17.03.0~ce-0~ubuntu-xenial | http://mirrors.aliyun.com/docker-ce/linux/ubuntu xenial/stable amd64 Packages
+# Step 2: Install Docker CE of a specified version: (The version is, for example, the preceding 17.03.1~ce-0~ubuntu-xenial)
+# sudo apt-get -y install docker-ce=[VERSION]
